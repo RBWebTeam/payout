48a87d8530533ad7146bc4f50bf04f33a6ed204b
manishdixit18 <manishdixit18@gmail.com>
2017-10-12 14:28:48 +0530

    pending page

--- app/Http/Controllers/MastersController.php
+++ app/Http/Controllers/MastersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 
-class ProductController extends Controller
+class MastersController extends Controller
 {
     public function products(){
      
---
+++ resources/views/pending-page.blade.php
@@ -0,0 +1,123 @@
+  @extends('includes.master')
+  @section('content')  
+ <div class="right_col" role="main">
+            <div class="row"  class="right_col"">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+              
+                  <div class="x_content" style="overflow-x:auto;">
+                    <p class="text-muted font-13 m-b-30">
+                   <b style="font-size: 30px">Product Type</b>
+                    </p>
+                    <table id="" class="table table-striped table-bordered table-responsive">
+                      <thead>
+                        <tr>
+                          <th>name</th>
+                          <th>fba_code</th>
+                          <th>fba_name</th>
+                          <th>sm_code</th>
+                          <th>sm_name</th>
+                          <th>posp_code</th>
+                          <th>dsa_code</th>
+                          <th>product_id</th>
+                          <th>status_id</th>
+                          <th>log_id</th>
+                          <th>sync_id</th>
+                          <th>create_date</th>
+                          <th>modify_date</th>
+                          <th>report_month</th>
+                          <th>report_date</th>
+                          <th>crn_code</th>
+                          <th>region</th>
+                          <th>area</th>
+                          <th>location</th>
+                          <th>business_mode</th>
+                          <th>type_of_entry</th>
+                          <th>cover_note_no</th>
+                          <th>inception_date</th>
+                          <th>transaction_date</th>
+                          <th>disbursal_date</th>
+                          <th>disbursal_data</th>
+                          <th>policy_code</th>
+                          <th>loan_no</th>
+                          <th>bank_nbfc</th>
+                          <th>client_name</th>
+                          <th>class_of_business</th>
+                          <th>type_of_business</th>
+                          <th>policy_no</th>
+                          <th>vehicle</th>
+                          <th>insurance_company</th>
+                          <th>tariff</th>
+                          <th>discount</th>
+                          <th>gross_premium</th>
+                          <th>premium_for_payout</th>
+                          <th>disbursal_for_payout</th>
+                          <th>payout</th>
+                          <th>gross_payable</th>
+                          <th>less_tds</th>
+                          <th>net_payable_after_tds</th>
+                          <th>final_commision_payable</th>
+                          <th>payment_in_favour</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                      	@foreach($payout_data as $value)
+                        <tr>
+                            <td>{{$value->name}}</td>
+							<td>{{$value->fba_code}}</td>
+							<td>{{$value->fba_name}}</td>
+							<td>{{$value->sm_code}}</td>
+							<td>{{$value->sm_name}}</td>
+							<td>{{$value->posp_code}}</td>
+							<td>{{$value->dsa_code}}</td>
+							<td>{{$value->product_id}}</td>
+							<td>{{$value->status_id}}</td>
+							<td>{{$value->log_id}}</td>
+							<td>{{$value->sync_id}}</td>
+							<td>{{$value->create_date}}</td>
+							<td>{{$value->modify_date}}</td>
+							<td>{{$value->report_month}}</td>
+							<td>{{$value->report_date}}</td>
+							<td>{{$value->crn_code}}</td>
+							<td>{{$value->region}}</td>
+							<td>{{$value->area}}</td>
+							<td>{{$value->location}}</td>
+							<td>{{$value->business_mode}}</td>
+							<td>{{$value->type_of_entry}}</td>
+							<td>{{$value->cover_note_no}}</td>
+							<td>{{$value->inception_date}}</td>
+							<td>{{$value->transaction_date}}</td>
+							<td>{{$value->disbursal_date}}</td>
+							<td>{{$value->disbursal_data}}</td>
+							<td>{{$value->policy_code}}</td>
+							<td>{{$value->loan_no}}</td>
+							<td>{{$value->bank_nbfc}}</td>
+							<td>{{$value->client_name}}</td>
+							<td>{{$value->class_of_business}}</td>
+							<td>{{$value->type_of_business}}</td>
+							<td>{{$value->policy_no}}</td>
+							<td>{{$value->vehicle}}</td>
+							<td>{{$value->insurance_company}}</td>
+							<td>{{$value->tariff}}</td>
+							<td>{{$value->discount}}</td>
+							<td>{{$value->gross_premium}}</td>
+							<td>{{$value->premium_for_payout}}</td>
+							<td>{{$value->disbursal_for_payout}}</td>
+							<td>{{$value->payout}}</td>
+							<td>{{$value->gross_payable}}</td>
+							<td>{{$value->less_tds}}</td>
+							<td>{{$value->net_payable_after_tds}}</td>
+							<td>{{$value->final_commision_payable}}</td>
+							<td>{{$value->payment_in_favour}}</td>
+                        </tr>
+                     @endforeach
+                        
+                      
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+   @endsection
