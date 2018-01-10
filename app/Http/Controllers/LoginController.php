b69de852549014dc02f6ee22dc73321dc50ac221
manishdixit18 <manishdixit18@gmail.com>
2018-01-10 16:11:29 +0530

    agent login with pwd encryption

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -20,6 +20,7 @@ class LoginController extends CallApiController
     }
 
     public function login_page(Request $request){
+        $is_authenticated=0;
     		$validator = Validator::make($request->all(), [
             'email' => 'required',
             'password' => 'required',
@@ -38,12 +39,18 @@ class LoginController extends CallApiController
           ->first();
           if($value){
             $this::set_session_for_user($value->id,$value->name,$value->email,$value->role_id,$value->product_type_id);
+            $is_authenticated=1;
           }else{
             $agent_from_api=$this::call_agent_api($request);
-            $this::set_session_for_user(15,$agent_from_api->Fullname,$agent_from_api->UserName,5,1);
-            return redirect('dashboard');
+            //print_r($agent_from_api);exit();
+            if($agent_from_api)
+               { 
+                  $this::set_session_for_user(15,$agent_from_api->Fullname,$agent_from_api->UserName,5,1);
+                  $is_authenticated=1;
+              }
+
           }
-          	if($value){ 
+          	if($is_authenticated){ 
                 return redirect('dashboard');
 
                 }else{
@@ -59,14 +66,27 @@ class LoginController extends CallApiController
 
     }
     public function call_agent_api($req){
-      $email="rajeev.ranjan9@rupeeboss.com";
-      $password="4mxQl4U4Kl+iOEkKNxTw1w==";
-      $data='{"UserName": "'.$email.'","Password": "'.$password.'"}';
-      $url="http://apiservices.magicfinmart.com/api/Login/ProcessLogin";
-      $result=$this::call_json_data_api($url,$data);
-      $http_result=json_decode($result['http_result'])[0];
+      $email=$req->email;
+      $password='{"Password":"'.$req->password.'"}';
+      $enc_url="http://49.50.95.141:192/PBService.svc/GetEncryptedPassword";
+      $pwd=($this::call_json_data_api($enc_url,$password));
+      $pwd_result=json_decode($pwd['http_result']);
+      $is_enc=$pwd_result->Status;
+      //print_r($pwd_result->EncryptPwd->EncryptedPassword);exit();
+      if($is_enc=='success'){
+          $data='{"UserName": "'.$email.'","Password": "'.$pwd_result->EncryptPwd->EncryptedPassword.'"}';
+          $url="http://apiservices.magicfinmart.com/api/Login/ProcessLogin";
+          $result=$this::call_json_data_api($url,$data);
+          $http_result=json_decode($result['http_result'])[0];
+          if($http_result->Result=='Success') 
+            return $http_result;
+      }else{
+        Session::flash('msg', "Password Encryption Failed ");
+      }
 
-      return $http_result;
+
+      
+      return "";
 
 
     }
