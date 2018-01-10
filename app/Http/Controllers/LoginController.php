a7486701a994f8b86ee3d2bddd2eb7c901d0ada1
manishdixit18 <manishdixit18@gmail.com>
2018-01-10 14:56:16 +0530

    agent login enabled

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,7 @@ use URL;
 use Mail;
 use DB;
 
-class LoginController extends Controller
+class LoginController extends CallApiController
 {
     public function login(){
       if(Session::get('userid')){
@@ -36,14 +36,15 @@ class LoginController extends Controller
           ->where('password','=', $request->password)
           //->where('user_type_id','!=','0')
           ->first();
+          if($value){
+            $this::set_session_for_user($value->id,$value->name,$value->email,$value->role_id,$value->product_type_id);
+          }else{
+            $agent_from_api=$this::call_agent_api($request);
+            $this::set_session_for_user(15,$agent_from_api->Fullname,$agent_from_api->UserName,5,1);
+            return redirect('dashboard');
+          }
           	if($value){ 
-                  $request->session()->put('userid',$value->id);
-		          	  $request->session()->put('name',$value->name);
-		          	  $request->session()->put('email',$value->email);
-		              $request->session()->put('role_id',$value->role_id);
-                  $request->session()->put('product_type_id',$value->product_type_id);
-
-                     return redirect('dashboard');
+                return redirect('dashboard');
 
                 }else{
                	 Session::flash('msg', "Invalid email or password. Please Try again! ");
@@ -56,5 +57,24 @@ class LoginController extends Controller
       Session::flush();
       return Redirect('/');
 
+    }
+    public function call_agent_api($req){
+      $email="rajeev.ranjan9@rupeeboss.com";
+      $password="4mxQl4U4Kl+iOEkKNxTw1w==";
+      $data='{"UserName": "'.$email.'","Password": "'.$password.'"}';
+      $url="http://apiservices.magicfinmart.com/api/Login/ProcessLogin";
+      $result=$this::call_json_data_api($url,$data);
+      $http_result=json_decode($result['http_result'])[0];
+
+      return $http_result;
+
+
+    }
+    public function set_session_for_user($id,$name,$email,$role_id,$product_type_id){
+        Session::put('userid',$id);
+        Session::put('name',$name);
+        Session::put('email',$email);
+        Session::put('role_id',$role_id);
+        Session::put('product_type_id',$product_type_id);
     }
 }
